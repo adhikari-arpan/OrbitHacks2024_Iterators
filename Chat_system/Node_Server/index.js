bc81f6de9f139d8f72d3fec4dfba08edddd62ef6
adhikari-arpan <adhikariarpan2063@gmail.com>
2024-11-29 00:17:15 +0545

    Update in server

--- Chat_system/Node_Server/index.js
+++ Chat_system/Node_Server/index.js
@@ -1,17 +1,24 @@
-//Node Server handling socket.io connections
+// Node Server handling socket.io connections
 
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
+
 // Enable CORS for all origins
 app.use(cors());
 
-const io = require('socket.io')(8000)
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*", // Allow all origins
+        methods: ["GET", "POST"]
+    }
+});
 
 const users = {};
 
 io.on('connection', socket => {
-    socket.on('new-user-joined', name => {d
+    socket.on('new-user-joined', name => {
         console.log("New user", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
@@ -21,8 +28,13 @@ io.on('connection', socket => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 
-    socket.on('disconnect', message => {
+    socket.on('disconnect', () => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
+});
+
+// Start the server
+server.listen(8000, () => {
+    console.log('Server is running on port 8000');
 });
--- Chat_system/client.js
+++ Chat_system/client.js
@@ -4,9 +4,22 @@ const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
 
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    // if(position == 'left'){
+    //     audio.play();
+    // }
+}
+
 const name = prompt("Enter your name to join");
 socket.emit('new-user-joined', name);
 
-console.log("Connected to server");
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right');
+});
 
 // var audio = new Audio('ting.mp3');
